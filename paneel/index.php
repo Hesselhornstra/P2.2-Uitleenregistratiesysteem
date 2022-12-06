e9d361d90168a3487a71059aa7f261f4677c178f
Hesselgamer <243421@edu.rocfriesepoort.nl>
2022-12-06 11:56:43 +0100

    update docu en code

--- paneel/index.php
+++ paneel/index.php
@@ -7,7 +7,7 @@ require $_SERVER['DOCUMENT_ROOT'].'/config.php';
 		<meta charset="UTF-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Home - Uitleenregistratiesysteem</title>
+		<title>Paneel - Uitleenregistratiesysteem</title>
 		<link rel="stylesheet" href="css/style.css">
 		<script src="js/main.js" defer></script>
 	</head>
