802e6b8cba3a1bfc0469cb440d845a346ccbb8b8
NightShot395 <112870866+NightShot395@users.noreply.github.com>
2022-12-08 15:24:24 +0100

    Update barcodeshit.php

--- barcodeshit.php
+++ barcodeshit.php
@@ -2,3 +2,22 @@
     <title>hello there</title>
     <h1>tester</h1>
 </html>
+
+                    <input type="text" name="naam" placeholder="Woonplaats" required><br><br>
+                    
+                    <button type="submit">Bestel</button>
+                    
+                    <?php
+require $_SERVER['DOCUMENT_ROOT'].'/config.php';
+
+$con->query("
+    INSERT INTO
+    barcode
+    (
+    naam,
+   barcode
+    '".$con->real_escape_string($_POST['naam'])."',
+    '".$con->real_escape_string($_POST['barcode'])."'
+    )
+");
+?>
