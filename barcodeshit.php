d730fc902534577c79fa31182cab0ea1af1b7895
NightShot395 <112870866+NightShot395@users.noreply.github.com>
2022-12-08 16:17:03 +0100

    Update barcodeshit.php

--- barcodeshit.php
+++ barcodeshit.php
@@ -3,11 +3,11 @@
     <h1>tester</h1>
 </html>
 <form method="POST">
-                    <input type="text" name="naam" placeholder="naam" required><br><br>
-                    
-                    <button type="submit">Bestel</button>
+    <input type="text" name="naam" placeholder="naam" required><br><br>
+    <button type="submit">Bestel</button>
 </form>
                     <?php
+                    $row = $con->query("SELECT * FROM bacode");
 require $_SERVER['DOCUMENT_ROOT'].'/config.php';
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
 $con->query("
@@ -15,8 +15,25 @@ $con->query("
     bacode
     (
     naam
+    ) VALUES
+    (
     '".$con->real_escape_string($_POST['naam'])."'
     )
 ");
     }
 ?>
+ <table border='1'>
+                
+                <thead>
+                    <tr>
+                        <th class="groote">Aantal</th>
+
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><?php echo $row['naam'] ?></td>
+                        <td><?php echo $row['barcode'] ?></td>
+                        
+                    </tr>
+                    </tbody>
