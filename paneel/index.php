c5cc6bd7cd3ead6482c64a33fd859d049ec1ca8b
tinnit0 <leusinkthomas@gmail.com>
2022-12-13 13:50:15 +0100

    paneel

--- paneel/index.php
+++ paneel/index.php
@@ -1,17 +1,35 @@
 <?php
-require $_SERVER['DOCUMENT_ROOT'].'/config.php';
+//require $_SERVER['DOCUMENT_ROOT'] . '/config.php';//
 ?>
 <!DOCTYPE html>
 <html lang="nl">
-	<head>
-		<meta charset="UTF-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Paneel - Uitleenregistratiesysteem</title>
-		<link rel="stylesheet" href="css/style.css">
-		<script src="js/main.js" defer></script>
-	</head>
-	<body>
 
-	</body>
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Paneel - Uitleenregistratiesysteem</title>
+	<link rel="stylesheet" href="css/paneel.css">
+	<script src="js/main.js" defer></script>
+</head>
+
+<body>
+	<p>uitloggen<p>
+	<hr>
+	<div style="groote">
+	<table border='1'>
+		<caption>Te laat</caption>
+		<thead>
+		    <tr>
+				<th>Naam</th>
+				<th>Mail</th>
+				<th>Datum</th>
+				<th>Artikel</th>
+				<th>Datum terug</th>
+				<th>specificaties</th>
+			</tr>
+		</thead>
+		<div>
+</body>
+
 </html>
