3a11b771f3296fbb55276e4d16755904908123b3
tinnit0 <leusinkthomas@gmail.com>
2022-12-13 14:04:24 +0100

    tabels

--- paneel/index.php
+++ paneel/index.php
@@ -9,27 +9,68 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Paneel - Uitleenregistratiesysteem</title>
-	<link rel="stylesheet" href="css/paneel.css">
+	<link rel="stylesheet" href="/css/paneel.css">
 	<script src="js/main.js" defer></script>
 </head>
 
 <body>
-	<p>uitloggen<p>
-	<hr>
-	<div style="groote">
-	<table border='1'>
-		<caption>Te laat</caption>
-		<thead>
-		    <tr>
-				<th>Naam</th>
-				<th>Mail</th>
-				<th>Datum</th>
-				<th>Artikel</th>
-				<th>Datum terug</th>
-				<th>specificaties</th>
-			</tr>
-		</thead>
-		<div>
+	<p class="groote">uitloggen
+	<p>
+		<hr>
+		<center>
+			<div style="groote">
+				<table border='1'>
+					<caption>Te laat</caption> </BR>
+					<thead>
+						<tr>
+							<th>Naam</th>
+							<th>Mail</th>
+							<th>Datum</th>
+							<th>Artikel</th>
+							<th>Datum terug</th>
+							<th>specificaties</th>
+						</tr>
+					</thead>
+				</table>
+				<div>
+		</center> <br><br><br><br>
+		<hr>
+		<center>
+			<div style="groote">
+				<table border='1'>
+					<caption>Vandaag Inleveren</caption> </BR>
+					<thead>
+						<tr>
+							<th>Naam</th>
+							<th>Mail</th>
+							<th>Datum</th>
+							<th>Artikel</th>
+							<th>Datum terug</th>
+							<th>specificaties</th>
+						</tr>
+					</thead>
+				</table>
+				<div>
+		</center> <br><br><br><br>
+		<hr>
+		<center>
+			<div style="groote">
+				<table border='1'>
+					<caption>Momenteel Uitgeleend</caption> </BR>
+					<thead>
+						<tr>
+							<th>Naam</th>
+							<th>Mail</th>
+							<th>Datum</th>
+							<th>Artikel</th>
+							<th>Datum terug</th>
+							<th>specificaties</th>
+						</tr>
+					</thead>
+				</table>
+				<div>
+		</center> <br><br><br><br>
+		<hr>
 </body>
 
 </html>
