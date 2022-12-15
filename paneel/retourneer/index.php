af31f199f3e324bae5eeda9e35210a390e93b3ab
Hesselgamer <243421@edu.rocfriesepoort.nl>
2022-12-15 15:29:59 +0100

    retourneer placeholder

---
+++ paneel/retourneer/index.php
@@ -0,0 +1,20 @@
+<?php
+require $_SERVER['DOCUMENT_ROOT'].'/config.php';
+?>
+<!DOCTYPE html>
+<html lang="nl">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	    <link rel="icon" type="image/x-icon" href="/img/logo.png">
+		<title>Retourneer - Uitleenregistratiesysteem</title>
+		<script src="https://kit.fontawesome.com/3304b0f2b7.js" crossorigin="anonymous"></script>
+		<link rel="stylesheet" href="/css/retourneer.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+	</head>
+	<body>
+		<div>
+			
+	</body>
+</html>
