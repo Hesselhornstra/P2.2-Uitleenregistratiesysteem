2713ea1a4717cd363059b7368ab0968cfa86794d
tinnit0 <leusinkthomas@gmail.com>
2022-12-20 13:56:08 +0100

    update tabel

--- paneel/index.php
+++ paneel/index.php
@@ -3,7 +3,7 @@ require $_SERVER['DOCUMENT_ROOT'] . '/config.php';
 /*if ($_SESSION['loggedin'] != true) {
 	Header("Location: /");
 }*/
-
+$artikel = $con->query("SELECT * FROM artikelen");
 ?>
 <!DOCTYPE html>
 <html lang="nl">
@@ -20,107 +20,116 @@ require $_SERVER['DOCUMENT_ROOT'] . '/config.php';
 
 <body>
 	<p class="grote">uitloggen</p>
-		<hr>
-		<center>
-			<div class="grote">
-				<table border='1'>
-					<caption>Te laat</caption> </BR>
-					<thead>
-						<tr>
-							<th>Naam</th>
-							<th>Mail</th>
-							<th>Datum</th>
-							<th>Artikel</th>
-							<th>Datum terug</th>
-							<th>specificaties</th>
-						</tr>
-					</thead>
-				</table>
-			</div>
-			<tbody>
-				<tr>
-					<td><?php echo $row['z'] ?></td>
-					<td><?php echo $row['z'] ?></td>
-					<td><?php echo $row['z'] ?></td>
-					<td><?php echo $row['z'] ?></td>
-					<td><?php echo $row['z'] ?></td>
-				    <td><?php echo $row['z'] ?></td>
-				</tr>
-			</tbody>
-		</center> <br><br><br><br>
-		<hr>
-		<center>
-			<div class="grote">
-				<table border='1'>
-					<caption>Vandaag Inleveren</caption> </BR>
-					<thead>
-						<tr>
-							<th>Naam</th>
-							<th>Mail</th>
-							<th>Datum</th>
-							<th>Artikel</th>
-							<th>Datum terug</th>
-							<th>specificaties</th>
-						</tr>
-					</thead>
-				</table>
-			</div>
-		</center> <br><br><br><br>
-		<hr>
-		<center>
-			<div class="grote">
-				<table border='1'>
-					<caption>Momenteel Uitgeleend</caption> </BR>
-					<thead>
-						<tr>
-							<th>Naam</th>
-							<th>Mail</th>
-							<th>Datum</th>
-							<th>Artikel</th>
-							<th>Datum terug</th>
-							<th>specificaties</th>
-						</tr>
-					</thead>
-				</table>
-			</div>
-		</center> <br><br><br><br>
-		<hr>
-		<center>
-			<form action="post">
-				<div class="bord">
-					<br>
-					<div>
-						<a class="boxol">Groep</a>
-						<a class="box">naam artikel</a>
-						<a class="box">Info artikel</a>
-						<a class="box">Foto artikel</a>
-						<br>><button class="boxton">Toevoegen</button>
-					</div>
-					<br>
-					<table class="tabl" border='1'>
-						<tr>
-							<th>Selecteer</th>
-						</tr>
-						<tr>
-							<th>Laptops</th>
-						</tr>
-						<tr>
-							<th>Tablets</th>
-						</tr>
-						<tr>
-							<th>monitoren</th>
-						</tr>
-						<tr>
-							<th>Camera's</th>
-						</tr>
-						<input class="txt" type="text">
-						<input class="txt" type="text">
-						<input class="txt" type="text">
-					</table>
+	<hr>
+	<center>
+		<div class="grote">
+			<table border='1'>
+				<caption>Te laat</caption> </BR>
+				<thead>
+					<tr>
+						<th>Naam</th>
+						<th>Mail</th>
+						<th>Datum</th>
+						<th>Artikel</th>
+						<th>Datum terug</th>
+						<th>specificaties</th>
+					</tr>
+				</thead>
+			</table>
+		</div>
+		<?php
+		while ($row = mysqli_fetch_assoc($artikel)) {
+		?>
+			<tr>
+				<td><?php echo $row["naam"]; ?></td>
+				<td><?php echo $row["mail"]; ?></td>
+				<td><?php echo $row["datum"]; ?></td>
+				<td><?php echo $row["artikel"]; ?></td>
+				<td><?php echo $row["datum terug"]; ?></td>
+				<td><?php echo $row["specificaties"]; ?></td>
+			</tr>
+		<?php
+		}
+		?>
+		</table>
+	</center> <br><br><br><br>
+	<hr>
+	<center>
+		<div class="grote">
+			<table border='1'>
+				<caption>Vandaag Inleveren</caption> </BR>
+				<thead>
+					<tr>
+						<th>Naam</th>
+						<th>Mail</th>
+						<th>Datum</th>
+						<th>Artikel</th>
+						<th>Datum terug</th>
+						<th>specificaties</th>
+					</tr>
+				</thead>
+			</table>
+		</div>
+	</center> <br><br><br><br>
+	<hr>
+	<center>
+		<div class="grote">
+			<table border='1'>
+				<caption>Momenteel Uitgeleend</caption> </BR>
+				<thead>
+					<tr>
+						<th>Naam</th>
+						<th>Mail</th>
+						<th>Datum</th>
+						<th>Artikel</th>
+						<th>Datum terug</th>
+						<th>specificaties</th>
+					</tr>
+				</thead>
+			</table>
+		</div>
+	</center> <br><br><br><br>
+	<hr>
+	<center>
+		<form action="post">
+			 
+			<div class="bord">
+				<br>
+				<div>
+					<a class="boxol">Groep</a>
+					<a class="box">naam artikel</a>
+					<a class="box">Info artikel</a>
+					<a class="box">Foto artikel</a>
+					<br>><button class="boxton">Toevoegen</button>
 				</div>
-		</center>
-		<br>
-		<center>
+				<br>
+				<table class="tabl" border='1'>
+					<tr>
+						<th>Selecteer</th>
+					</tr>
+					<tr>
+						<th>Laptops</th>
+					</tr>
+					<tr>
+						<th>Tablets</th>
+					</tr>
+					<tr>
+						<th>monitoren</th>
+					</tr>
+					<tr>
+						<th>Camera's</th>
+					</tr>
+					<input class="txt" type="text">
+					<input class="txt" type="text">
+					<input class="txt" type="text">
+				</table>
+			</div>
+	</center>
+	<br>
+	<center>
+		<?php
+			$query = "UPDATE artikelen SET name=?, info=?, img=? WHERE id=?";
+			?>
 			<div class="bord">
 				<br>
 				<div>
@@ -152,41 +161,41 @@ require $_SERVER['DOCUMENT_ROOT'] . '/config.php';
 					<input class="txt" type="text">
 				</table>
 			</div>
-		</center>
-		<br>
-		<center>
-			<div class="bord">
-				<br>
-				<div>
-					<a class="boxol">Groep</a>
-					<a class="box">naam artikel</a>
-					<a class="box">Info artikel</a>
-					<a class="box">Foto artikel</a>
-					<br><button class="boxton">Verwijderen</button>
-				</div>
-				<br>
-				<table class="tabl" border='1'>
-					<tr>
-						<th>Selecteer</th>
-					</tr>
-					<tr>
-						<th>Laptops</th>
-					</tr>
-					<tr>
-						<th>Tablets</th>
-					</tr>
-					<tr>
-						<th>monitoren</th>
-					</tr>
-					<tr>
-						<th>Camera's</th>
-					</tr>
-					<input class="txt" type="text">
-					<input class="txt" type="Text">
-					<input class="txt" type="text">
-				</table>
+	</center>
+	<br>
+	<center>
+		<div class="bord">
+			<br>
+			<div>
+				<a class="boxol">Groep</a>
+				<a class="box">naam artikel</a>
+				<a class="box">Info artikel</a>
+				<a class="box">Foto artikel</a>
+				<br><button class="boxton">Verwijderen</button>
 			</div>
-		</center>
+			<br>
+			<table class="tabl" border='1'>
+				<tr>
+					<th>Selecteer</th>
+				</tr>
+				<tr>
+					<th>Laptops</th>
+				</tr>
+				<tr>
+					<th>Tablets</th>
+				</tr>
+				<tr>
+					<th>monitoren</th>
+				</tr>
+				<tr>
+					<th>Camera's</th>
+				</tr>
+				<input class="txt" type="text">
+				<input class="txt" type="Text">
+				<input class="txt" type="text">
+			</table>
+		</div>
+	</center>
 </body>
 
 </html>
