b657616e0c1c47845cdd3334ca488ad8ce9d968f
NightShot395 <112870866+NightShot395@users.noreply.github.com>
2022-12-21 12:55:19 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ function eateregg(){
     console.log("Eateregg");
     setTimeout(function(){
       var the_box = document.getElementById("eateregg");
-      var the_blc = document.getElementById("eateregg");
+      var the_blc = document.getElementById("eastteregg");
     var the_but = document.getElementById("eateregb");
     if (the_box.classList.contains("easteregg")) {
       the_box.classList.remove("easteregg");
