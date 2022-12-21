5092b081bf6c3b041cc76b7f7ef3105a0688160d
NightShot395 <112870866+NightShot395@users.noreply.github.com>
2022-12-21 12:52:42 +0100

    s

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 function eateregg(){
     console.log("Eateregg");
     setTimeout(function(){
-    var the_box = document.getElementById("eateregg");
+      var the_box = document.getElementById("eateregg");
+      var the_blc = document.getElementById("eateregg");
     var the_but = document.getElementById("eateregb");
     if (the_box.classList.contains("easteregg")) {
       the_box.classList.remove("easteregg");
       the_but.classList.remove("easteregg");
+      the_blc.classlist.remove("eateregg");
     } else {
       the_box.classList.add("easteregg");
       the_but.classList.add("easteregg");
