e364e641b35b12cb6d5145d456a87b12d502fe78
NightShot395 <112870866+NightShot395@users.noreply.github.com>
2023-01-09 15:25:14 +0100

    Update config.php

--- config.php
+++ config.php
@@ -6,8 +6,8 @@ date_default_timezone_set('Europe/Amsterdam');
 ini_set("max_execution_time",500);
 
 $db_host = "localhost";
-$db_user = "root";
-$db_pass = "";
+$db_user = "deb85590_ p22k1t4";
+$db_pass = "dADbg3ql7";
 $db_data = "uitleensysteem";
 
 $con = new mysqli($db_host,$db_user,$db_pass,$db_data);
